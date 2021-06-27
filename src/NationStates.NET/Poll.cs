1737c222c65af79a0a491d407e774305dfa158e4
asdia0 <asdia.the.potato@gmail.com>
2021-06-27 23:56:56 +0800

    Add Poll(long)

--- src/NationStates.NET/Poll.cs
+++ src/NationStates.NET/Poll.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml;
 
     /// <summary>
     /// Represents a regional poll.
@@ -62,6 +64,35 @@
             this.Stop = stop;
             this.Author = author;
             this.Options = options;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Poll"/> class.
+        /// </summary>
+        /// <param name="id">The poll's ID.</param>
+        public Poll(long id)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(Utility.DownloadUrlString($"https://www.nationstates.net/cgi-bin/api.cgi?q=poll;pollid={id}"));
+
+            XmlNode node = doc.DocumentElement.SelectSingleNode("POLL");
+
+            this.Title = node.SelectSingleNode("TITLE").InnerText;
+            this.Start = Utility.ParseUnix(node.SelectSingleNode("START").InnerText);
+            this.Stop = Utility.ParseUnix(node.SelectSingleNode("STOP").InnerText);
+            this.Author = node.SelectSingleNode("AUTHOR").InnerText;
+            this.Options = new HashSet<PollOption>();
+
+            foreach (XmlNode option in node.SelectSingleNode("OPTIONS").ChildNodes)
+            {
+                int optionID = int.Parse(option.Attributes["id"].Value);
+                string text = option.SelectSingleNode("OPTIONTEXT").InnerText;
+                int votes = int.Parse(option.SelectSingleNode("VOTES").InnerText);
+                HashSet<string> voters = option.SelectSingleNode("VOTERS").InnerText.Split(":").ToHashSet();
+
+                this.Options.Add(new PollOption(optionID, text, votes, voters));
+            }
         }
     }
 }
