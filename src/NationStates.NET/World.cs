35da2f8235616d313fb62ba49faab301b5f60c09
asdia0 <asdia.the.potato@gmail.com>
2021-06-28 07:57:01 +0800

    Add World.GetBanner()

--- src/NationStates.NET/World.cs
+++ src/NationStates.NET/World.cs
@@ -10,6 +10,33 @@
     public static class World
     {
         /// <summary>
+        /// Gets the name and validity of banners from their ID.
+        /// </summary>
+        /// <param name="banners">A collection of the banner IDs to search for.</param>
+        /// <returns>The name and validty of specified banners.</returns>
+        public static Dictionary<string, (string Name, string Validity)> GetBanner(HashSet<string> banners)
+        {
+            Dictionary<string, (string Name, string Validity)> res = new Dictionary<string, (string Name, string Validity)>();
+
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(Utility.DownloadUrlString($"https://www.nationstates.net/cgi-bin/api.cgi?q=banner;banner={string.Join(",", banners)}"));
+
+            XmlNode node = doc.DocumentElement.SelectSingleNode("BANNERS");
+
+            foreach (XmlNode banner in node.ChildNodes)
+            {
+                string id = banner.Attributes["id"].Value;
+                string name = banner.SelectSingleNode("NAME").InnerText;
+                string validity = banner.SelectSingleNode("VALIDITY").InnerText;
+
+                res.Add(id, (name, validity));
+            }
+
+            return res;
+        }
+
+        /// <summary>
         /// Gets a dispatch from its ID.
         /// </summary>
         /// <param name="id">The dispatch's ID.</param>
