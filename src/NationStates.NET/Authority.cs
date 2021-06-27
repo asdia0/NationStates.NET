a052eb97cdb0bb6da8e3be83113b8186385069c8
asdia0 <asdia.the.potato@gmail.com>
2021-06-27 21:03:36 +0800

    Add documentation to Authority

--- src/NationStates.NET/Authority.cs
+++ src/NationStates.NET/Authority.cs
@@ -5,12 +5,39 @@
     /// </summary>
     public enum Authority
     {
+        /// <summary>
+        /// Has executive powers.
+        /// </summary>
         Executive,
+
+        /// <summary>
+        /// Has world assembly powers (WA Delegate).
+        /// </summary>
         World_Assembly,
+
+        /// <summary>
+        /// Has appearance powers (factbook, tags, etc).
+        /// </summary>
         Appearance,
+
+        /// <summary>
+        /// Has border control powers (password).
+        /// </summary>
         Border_Control,
+
+        /// <summary>
+        /// Has communications powers (RMB).
+        /// </summary>
         Communications,
+
+        /// <summary>
+        /// Has embassy powers.
+        /// </summary>
         Embassies,
+
+        /// <summary>
+        /// Has poll powers.
+        /// </summary>
         Polls,
     }
 }
