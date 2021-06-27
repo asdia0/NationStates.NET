561a15a366b91e5300fc005a74dfbd660a79b976
asdia0 <asdia.the.potato@gmail.com>
2021-06-27 20:54:52 +0800

    Add World class

---
+++ src/NationStates.NET/World.cs
@@ -0,0 +1,14 @@
+﻿namespace NationStates.NET
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    public static class World
+    {
+        public static Dispatch GetDispatch(ulong id)
+        {
+            return new Dispatch(id);
+        }
+    }
+}
