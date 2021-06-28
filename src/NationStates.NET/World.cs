53f3ce9d829b3395365d82c0874e04636964eee8
asdia0 <asdia.the.potato@gmail.com>
2021-06-28 22:30:41 +0800

    Add World.GetCensusTitle(int)

--- src/NationStates.NET/World.cs
+++ src/NationStates.NET/World.cs
@@ -129,6 +129,20 @@
         }
 
         /// <summary>
+        /// Gets the title of the census.
+        /// </summary>
+        /// <param name="id">The ID of the census. </param>
+        /// <returns>The title of the specified census.</returns>
+        public static string GetCensusTitle(int id)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(Utility.DownloadUrlString($"https://www.nationstates.net/cgi-bin/api.cgi?q=censustitle;scale={id}"));
+
+            return doc.DocumentElement.FirstChild.InnerText;
+        }
+
+        /// <summary>
         /// Gets a dispatch from its ID.
         /// </summary>
         /// <param name="id">The dispatch's ID.</param>
