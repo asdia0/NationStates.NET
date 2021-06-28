964d9bff5f1837caae4943afd50889ae8460bca8
asdia0 <asdia.the.potato@gmail.com>
2021-06-28 10:01:52 +0800

    Change NationZombie from class to struct

--- src/NationStates.NET/NationZombie.cs
+++ src/NationStates.NET/NationZombie.cs
@@ -1,37 +1,37 @@
 ﻿namespace NationStates.NET
 {
     /// <summary>
-    /// Represents a nation during Z-Day.
+    /// Defines a nation during Z-Day.
     /// </summary>
-    public class NationZombie
+    public struct NationZombie
     {
         /// <summary>
-        /// Gets or sets the nation's action.
+        /// Gets the nation's action.
         /// </summary>
-        public ZombieAction Action { get; set; }
+        public ZombieAction Action { get; }
 
         /// <summary>
-        /// Gets or sets the nation's intended action. If null, see <see cref="Action"/>.
+        /// Gets the nation's intended action. If null, see <see cref="Action"/>.
         /// </summary>
-        public ZombieAction? IntendedAction { get; set; }
+        public ZombieAction? IntendedAction { get; }
 
         /// <summary>
-        /// Gets or sets the number of survivors in millions.
+        /// Gets the number of survivors in millions.
         /// </summary>
-        public long Survivors { get; set; }
+        public long Survivors { get; }
 
         /// <summary>
-        /// Gets or sets the number of zombies in millions.
+        /// Gets the number of zombies in millions.
         /// </summary>
-        public long Zombies { get; set; }
+        public long Zombies { get; }
 
         /// <summary>
-        /// Gets or sets the number of dead people in millions.
+        /// Gets the number of dead people in millions.
         /// </summary>
-        public long Dead { get; set; }
+        public long Dead { get; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="NationZombie"/> class.
+        /// Initializes a new instance of the <see cref="NationZombie"/> struct.
         /// </summary>
         /// <param name="action">Nation's action.</param>
         /// <param name="intendedAction">Nation;s intended action.</param>
