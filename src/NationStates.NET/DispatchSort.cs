9b5f5a538b9529913a3716bd40d35d69b870044c
asdia0 <asdia.the.potato@gmail.com>
2021-06-28 22:34:24 +0800

    Add DispatchSort

---
+++ src/NationStates.NET/DispatchSort.cs
@@ -0,0 +1,18 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of dispatch sorts.
+    /// </summary>
+    public enum DispatchSort
+    {
+        /// <summary>
+        /// Sort by new.
+        /// </summary>
+        New,
+
+        /// <summary>
+        /// Sort by best.
+        /// </summary>
+        Best,
+    }
+}
