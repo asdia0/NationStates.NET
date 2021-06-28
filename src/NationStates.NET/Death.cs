da38d6051330ed7201814e4410466b0d633b6817
asdia0 <asdia.the.potato@gmail.com>
2021-06-28 09:56:16 +0800

    Change Death from class to struct

--- src/NationStates.NET/Death.cs
+++ src/NationStates.NET/Death.cs
@@ -1,45 +1,32 @@
 ﻿namespace NationStates.NET
 {
     /// <summary>
-    /// Represents a cause of death.
+    /// Defines a cause of death.
     /// </summary>
-    public class Death
+    public struct Death
     {
-        private double _frequency;
-
         /// <summary>
-        /// Gets or sets the cause of death.
+        /// Gets the cause of death.
         /// </summary>
-        public string Cause { get; set; }
+        public string Cause { get; }
 
         /// <summary>
-        /// Gets or sets the death's national frequency in percentage.
+        /// Gets the death's national frequency in percentage.
         /// </summary>
-        public double Frequency
-        {
-            get
-            {
-                return this._frequency;
-            }
-
-            set
-            {
-                if (value > 100 || value < 0)
-                {
-                    throw new NSError("Frequency must be in the interval [0, 100].");
-                }
-
-                this._frequency = value;
-            }
-        }
+        public double Frequency { get; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Death"/> class.
+        /// Initializes a new instance of the <see cref="Death"/> struct.
         /// </summary>
         /// <param name="cause">Cause of death.</param>
         /// <param name="frequency">Frequency in percentage.</param>
         public Death(string cause, double frequency)
         {
+            if (frequency > 100 || frequency < 0)
+            {
+                throw new NSError("Frequency must be in the interval [0, 100].");
+            }
+
             this.Cause = cause;
             this.Frequency = frequency;
         }
