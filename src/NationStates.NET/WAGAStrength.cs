a8a471d6a17a97976448d356932a420cbccc5a21
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:19:10 +0800

    Add WAGAStrength

---
+++ src/NationStates.NET/WAGAStrength.cs
@@ -0,0 +1,23 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of subcategories for the categories <see cref="WAGACategory.Civil_Rights"/>, <see cref="WAGACategory.Free_Trade"/>, <see cref="WAGACategory.Furtherment_Of_Democracy"/>, <see cref="WAGACategory.Global_Disarmament"/>, <see cref="WAGACategory.International_Security"/>, <see cref="WAGACategory.Moral_Decency"/>, <see cref="WAGACategory.Political_Stability"/> and <see cref="WAGACategory.Social_Justice"/>.
+    /// </summary>
+    public enum WAGAStrength
+    {
+        /// <summary>
+        /// Mild.
+        /// </summary>
+        Mild,
+
+        /// <summary>
+        /// Significant.
+        /// </summary>
+        Significant,
+
+        /// <summary>
+        /// Strong.
+        /// </summary>
+        Strong,
+    }
+}
