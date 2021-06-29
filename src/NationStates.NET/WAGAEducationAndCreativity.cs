de490f284b4fe31c7feb96e7964cec4382a93afb
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:21:36 +0800

    Add WAGAEducationAndCreativity

---
+++ src/NationStates.NET/WAGAEducationAndCreativity.cs
@@ -0,0 +1,28 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of subcategories for the category <see cref="WAGACategory.Education_And_Creativity"/>.
+    /// </summary>
+    public enum WAGAEducationAndCreativity
+    {
+        /// <summary>
+        /// Artistic.
+        /// </summary>
+        Artistic,
+
+        /// <summary>
+        /// Educational.
+        /// </summary>
+        Educational,
+
+        /// <summary>
+        /// Cultural heritage.
+        /// </summary>
+        Cultural_Heritage,
+
+        /// <summary>
+        /// Free press.
+        /// </summary>
+        Free_Press,
+    }
+}
