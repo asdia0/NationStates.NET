c210885bdef7d85388b415e6761f6ea655970dc3
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:58:20 +0800

    Remove unneccessary Poll initializer

--- src/NationStates.NET/Poll.cs
+++ src/NationStates.NET/Poll.cs
@@ -49,27 +49,6 @@
         /// Initializes a new instance of the <see cref="Poll"/> struct.
         /// </summary>
         /// <param name="id">The poll's ID.</param>
-        /// <param name="title">The poll's title.</param>
-        /// <param name="region">The region the poll was held in.</param>
-        /// <param name="start">The time at which the poll started.</param>
-        /// <param name="stop">The time at which the poll ended.</param>
-        /// <param name="author">The name of the nation that started the poll.</param>
-        /// <param name="options">A list of options and results for the polls.</param>
-        public Poll(long id, string title, string region, DateTime start, DateTime stop, string author, HashSet<PollOption> options)
-        {
-            this.ID = id;
-            this.Title = title;
-            this.Region = region;
-            this.Start = start;
-            this.Stop = stop;
-            this.Author = author;
-            this.Options = options;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Poll"/> struct.
-        /// </summary>
-        /// <param name="id">The poll's ID.</param>
         public Poll(long id)
         {
             this.ID = id;
