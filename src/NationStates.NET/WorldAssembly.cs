9b7696ae2658c4b1e51713348d2f60d7410a3879
asdia0 <asdia.the.potato@gmail.com>
2021-06-30 01:17:35 +0800

    Add docs to WorldAssembly.GetResolution()

--- src/NationStates.NET/WorldAssembly.cs
+++ src/NationStates.NET/WorldAssembly.cs
@@ -5,6 +5,12 @@
     /// </summary>
     public static class WorldAssembly
     {
+        /// <summary>
+        /// Gets a World Assembly resolution.
+        /// </summary>
+        /// <param name="council">The council the resolution was submitted in.</param>
+        /// <param name="councilID">The resolution's council ID.</param>
+        /// <returns>A World Assembly resolution with the specified council ID.</returns>
         public static WAResolution GetResolution(WACouncil council, long councilID)
         {
             return new WAResolution(council, councilID);
