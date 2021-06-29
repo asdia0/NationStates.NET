8cfcb5721da055e7a637eade3f4c10cf8edd9ceb
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:26:49 +0800

    Add WAGARegulation

---
+++ src/NationStates.NET/WAGARegulation.cs
@@ -0,0 +1,38 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of subcategories for the category <see cref="WAGACategory.Regulation"/>.
+    /// </summary>
+    public enum WAGARegulation
+    {
+        /// <summary>
+        /// Consumer protection.
+        /// </summary>
+        Consumer_Protection,
+
+        /// <summary>
+        /// Transportation.
+        /// </summary>
+        Transportation,
+
+        /// <summary>
+        /// Safety.
+        /// </summary>
+        Safety,
+
+        /// <summary>
+        /// Labour rights.
+        /// </summary>
+        Labour_Rights,
+
+        /// <summary>
+        /// Energy.
+        /// </summary>
+        Energy,
+
+        /// <summary>
+        /// Legal reform.
+        /// </summary>
+        Legal_Reform,
+    }
+}
