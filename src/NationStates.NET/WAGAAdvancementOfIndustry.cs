2020fec606edad687897eef79c5667f55eeb4f6a
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:15:53 +0800

    Add WAGAAdvancementOfIndustry

---
+++ src/NationStates.NET/WAGAAdvancementOfIndustry.cs
@@ -0,0 +1,33 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of subcategories for the category <see cref="WAGACategory.Advancement_Of_Industry"/>.
+    /// </summary>
+    public enum WAGAAdvancementOfIndustry
+    {
+        /// <summary>
+        /// Environmental deregulation.
+        /// </summary>
+        Environmental_Deregulation,
+
+        /// <summary>
+        /// Labor deregulation.
+        /// </summary>
+        Labor_Deregulation,
+
+        /// <summary>
+        /// Protective tariffs.
+        /// </summary>
+        Protective_Tariffs,
+
+        /// <summary>
+        /// Tort reform.
+        /// </summary>
+        Tort_Reform,
+
+        /// <summary>
+        /// Commerical enterprise.
+        /// </summary>
+        Commerical_Enterprise,
+    }
+}
