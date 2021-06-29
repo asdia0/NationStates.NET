a2baf853362f8a8b9002dbf7a769698ad7b62a59
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 08:28:00 +0800

    Add EntityType

---
+++ src/NationStates.NET/EntityType.cs
@@ -0,0 +1,18 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of entities.
+    /// </summary>
+    public enum EntityType
+    {
+        /// <summary>
+        /// A nation.
+        /// </summary>
+        Nation,
+
+        /// <summary>
+        /// A region.
+        /// </summary>
+        Region,
+    }
+}
