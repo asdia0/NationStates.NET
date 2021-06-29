d809b1b4c76e903818fd4e09b564dbef81976756
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 23:04:19 +0800

    Revamp Freedom

--- src/NationStates.NET/Freedom.cs
+++ src/NationStates.NET/Freedom.cs
@@ -1,5 +1,8 @@
 ﻿namespace NationStates.NET
 {
+    using System;
+    using System.Xml;
+
     /// <summary>
     /// Defines a nation's freedom levels.
     /// </summary>
@@ -23,14 +26,18 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="Freedom"/> struct.
         /// </summary>
-        /// <param name="civilRights">Civil rights level.</param>
-        /// <param name="economy">Economy level.</param>
-        /// <param name="politicalFreedoms">Political freedoms level.</param>
-        public Freedom(CivilRights civilRights, Economy economy, PoliticalFreedoms politicalFreedoms)
+        /// <param name="nation">The nation's name.</param>
+        public Freedom(string nation)
         {
-            this.CivilRights = civilRights;
-            this.Economy = economy;
-            this.PoliticalFreedoms = politicalFreedoms;
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(Utility.DownloadUrlString($"https://www.nationstates.net/cgi-bin/api.cgi?nation={nation.Replace(" ", "_")}&q=freedom"));
+
+            XmlNode node = doc.DocumentElement.FirstChild;
+
+            this.CivilRights = (CivilRights)Enum.Parse(typeof(CivilRights), Utility.FormatForEnum(node.SelectSingleNode("CIVILRIGHTS").InnerText));
+            this.Economy = (Economy)Enum.Parse(typeof(Economy), Utility.FormatForEnum(node.SelectSingleNode("ECONOMY").InnerText));
+            this.PoliticalFreedoms = (PoliticalFreedoms)Enum.Parse(typeof(PoliticalFreedoms), Utility.FormatForEnum(node.SelectSingleNode("POLITICALFREEDOM").InnerText));
         }
     }
 }
