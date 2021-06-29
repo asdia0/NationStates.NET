4a77f2c012bc83057246ddc9ac6114ff1bd794a3
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:04:31 +0800

    Add WACouncil

---
+++ src/NationStates.NET/WACouncil.cs
@@ -0,0 +1,18 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of World Assembly councils.
+    /// </summary>
+    public enum WACouncil
+    {
+        /// <summary>
+        /// The General Assembly.
+        /// </summary>
+        General_Assembly,
+
+        /// <summary>
+        /// The Security Council.
+        /// </summary>
+        Security_Council,
+    }
+}
