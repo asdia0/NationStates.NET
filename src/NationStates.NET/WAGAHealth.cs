6b6842ae70f7c28d0bd396d2e5d080aa88a08b19
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:25:27 +0800

    Add WAGAHealth

---
+++ src/NationStates.NET/WAGAHealth.cs
@@ -0,0 +1,28 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of subcategories for the category <see cref="WAGACategory.Health"/>.
+    /// </summary>
+    public enum WAGAHealth
+    {
+        /// <summary>
+        /// Healthcare.
+        /// </summary>
+        Healthcare,
+
+        /// <summary>
+        /// International aid.
+        /// </summary>
+        International_Aid,
+
+        /// <summary>
+        /// Research.
+        /// </summary>
+        Research,
+
+        /// <summary>
+        /// Bioethics.
+        /// </summary>
+        Bioethics,
+    }
+}
