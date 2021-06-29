c3f872641013ab9584866f36f85d6aa22469bfa1
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:29:31 +0800

    Add WAGABookeeping

---
+++ src/NationStates.NET/WAGABookeeping.cs
@@ -0,0 +1,13 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of subcategories for the category <see cref="WAGACategory.Bookkeeping"/>.
+    /// </summary>
+    public enum WAGABookeeping
+    {
+        /// <summary>
+        /// Sweeping.
+        /// </summary>
+        Sweeping,
+    }
+}
