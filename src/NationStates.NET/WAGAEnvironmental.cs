d98777d806479b3f150ef3e48c282f4a2a314cd7
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:24:21 +0800

    Add WAGAEnvironmental

---
+++ src/NationStates.NET/WAGAEnvironmental.cs
@@ -0,0 +1,48 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of subcategories for the category <see cref="WAGACategory.Environmental"/>.
+    /// </summary>
+    public enum WAGAEnvironmental
+    {
+        /// <summary>
+        /// Automotive.
+        /// </summary>
+        Automotive,
+
+        /// <summary>
+        /// Mining.
+        /// </summary>
+        Mining,
+
+        /// <summary>
+        /// Logging.
+        /// </summary>
+        Logging,
+
+        /// <summary>
+        /// All businesses - strong.
+        /// </summary>
+        All_BusinessesStrong,
+
+        /// <summary>
+        /// Manufacturing.
+        /// </summary>
+        Manufacturing,
+
+        /// <summary>
+        /// Agriculture.
+        /// </summary>
+        Agriculture,
+
+        /// <summary>
+        /// Fishing.
+        /// </summary>
+        Fishing,
+
+        /// <summary>
+        /// All businesses - mild.
+        /// </summary>
+        All_BusinessesMild,
+    }
+}
