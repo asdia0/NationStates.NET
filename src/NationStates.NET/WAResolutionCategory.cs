0064288000851d7aa3176a707c1345297e79b219
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 22:03:11 +0800

    Add WAResolutionCategory

---
+++ src/NationStates.NET/WAResolutionCategory.cs
@@ -0,0 +1,92 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of World Assembly resolution categories.
+    /// </summary>
+    public enum WAResolutionCategory
+    {
+        // General Assembly
+
+        /// <summary>
+        /// Advancement of industry.
+        /// </summary>
+        Advancement_Of_Industry,
+
+        /// <summary>
+        /// Civil rights.
+        /// </summary>
+        Civil_Rights,
+
+        /// <summary>
+        /// Education and creativity.
+        /// </summary>
+        Education_And_Creativity,
+
+        /// <summary>
+        /// Environmental.
+        /// </summary>
+        Environmental,
+
+        /// <summary>
+        /// Free trade.
+        /// </summary>
+        Free_Trade,
+
+        /// <summary>
+        /// Furtherment of democracy.
+        /// </summary>
+        Furtherment_Of_Democracy,
+
+        /// <summary>
+        /// Global disarmament.
+        /// </summary>
+        Global_Disarmament,
+
+        /// <summary>
+        /// Health.
+        /// </summary>
+        Health,
+
+        /// <summary>
+        /// International security.
+        /// </summary>
+        International_Security,
+
+        /// <summary>
+        /// Moral decency.
+        /// </summary>
+        Moral_Decency,
+
+        /// <summary>
+        /// Political stability.
+        /// </summary>
+        Political_Stability,
+
+        /// <summary>
+        /// Regulation.
+        /// </summary>
+        Regulation,
+
+        /// <summary>
+        /// Social justice.
+        /// </summary>
+        Social_Justice,
+
+        // Security Council
+
+        /// <summary>
+        /// Conmmendation.
+        /// </summary>
+        Commendation,
+
+        /// <summary>
+        /// Condemnation.
+        /// </summary>
+        Condemnation,
+
+        /// <summary>
+        /// Liberation.
+        /// </summary>
+        Liberation,
+    }
+}
