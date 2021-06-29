192fe8047e52fa5e558ead96973f56a2b48b61f1
asdia <asdia.the.potato@gmail.com>
2021-06-29 11:07:33 +0800

    Edit UserAgent

--- src/NationStates.NET/Utility.cs
+++ src/NationStates.NET/Utility.cs
@@ -31,7 +31,7 @@
         {
             using (WebClient client = new WebClient())
             {
-                client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
+                client.Headers.Add("user-agent", "NationStates.NET 1.0 (https://github.com/asdia0/NationStates.NET)");
 
                 return client.DownloadString(url);
             }
