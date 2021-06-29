6a490e9e04922e28dde54e583d9a8e04542fee19
asdia0 <asdia.the.potato@gmail.com>
2021-06-29 10:34:53 +0800

    Add WorldCensus.Rank

--- src/NationStates.NET/WorldCensus.cs
+++ src/NationStates.NET/WorldCensus.cs
@@ -21,16 +21,23 @@
         public double Score { get; }
 
         /// <summary>
+        /// Gets the nation's rank.
+        /// </summary>
+        public long Rank { get; }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="WorldCensus"/> struct.
         /// </summary>
         /// <param name="id">The census ID.</param>
         /// <param name="name">The nation's name.</param>
         /// <param name="score">The nation's score.</param>
-        public WorldCensus(int id, string name, double score)
+        /// <param name="rank">The nation's rank in the world.</param>
+        public WorldCensus(int id, string name, double score, long rank)
         {
             this.ID = id;
             this.Name = name;
             this.Score = score;
+            this.Rank = rank;
         }
     }
 }
