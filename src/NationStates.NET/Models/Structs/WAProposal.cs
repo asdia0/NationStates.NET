2e282ace5be47368240f78efe69e0f6489dc3fc0
asdia0 <asdia.the.potato@gmail.com>
2021-11-04 20:31:01 +0800

    Add WAProposal

---
+++ src/NationStates.NET/Models/Structs/WAProposal.cs
@@ -0,0 +1,82 @@
+﻿namespace NationStates.NET
+{
+    using System;
+    using System.Collections.Generic;
+    using static Utility;
+
+    /// <summary>
+    /// Defines a World Assembly proposal.
+    /// </summary>
+    public struct WAProposal
+    {
+        /// <summary>
+        /// Gets the proposal's ID.
+        /// </summary>
+        public string ID { get; }
+
+        /// <summary>
+        /// Gets a list of delegates that approved of the proposal.
+        /// </summary>
+        public HashSet<string> Approvals { get; }
+
+        /// <summary>
+        /// Gets the proposal's category.
+        /// </summary>
+        public dynamic Category { get; }
+
+        /// <summary>
+        /// Gets the council the proposal was submitted in.
+        /// </summary>
+        public WACouncil Council { get; }
+
+        /// <summary>
+        /// Gets the time at which the proposal was created.
+        /// </summary>
+        public DateTime Created { get; }
+
+        /// <summary>
+        /// Gets the body of the proposal.
+        /// </summary>
+        public string Description { get; }
+
+        /// <summary>
+        /// Gets the proposal's name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the name of the nation that proposed the proposal.
+        /// </summary>
+        public string Proposer { get; }
+
+        /// <summary>
+        /// Gets the proposal's sub-category.
+        /// </summary>
+        public dynamic SubCategory { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WAProposal"/> struct.
+        /// </summary>
+        /// <param name="id">The proposal's ID.</param>
+        /// <param name="approvals">A list of delegates that approved of the proposal.</param>
+        /// <param name="category">The proposal's category.</param>
+        /// <param name="council">The council in which the proposal was submitted in.</param>
+        /// <param name="created">The time at which the proposal was created.</param>
+        /// <param name="description">The body of the proposal.</param>
+        /// <param name="name">The proposal's name.</param>
+        /// <param name="proposer">The name of the nation that proposed the proposal.</param>
+        /// <param name="subCategory">The proposal's sub-category.</param>
+        public WAProposal(string id, HashSet<string> approvals, dynamic category, WACouncil council, DateTime created, string description, string name, string proposer, dynamic subCategory)
+        {
+            this.ID = id;
+            this.Approvals = approvals;
+            this.Category = category;
+            this.Council = council;
+            this.Created = created;
+            this.Description = description;
+            this.Name = name;
+            this.Proposer = proposer;
+            this.SubCategory = subCategory;
+        }
+    }
+}
