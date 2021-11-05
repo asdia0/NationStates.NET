adde0ee49e41a40c7a8cc1dc8efafdfd0c09dcfd
asdia0 <asdia.the.potato@gmail.com>
2021-11-05 19:49:48 +0800

    Add WAAction

---
+++ src/NationStates.NET/Enums/WAAction.cs
@@ -0,0 +1,23 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Types of actions delegates can do to the <see cref="WAProposalAtVote"/>.
+    /// </summary>
+    public enum WAAction
+    {
+        /// <summary>
+        /// Against.
+        /// </summary>
+        Against,
+
+        /// <summary>
+        /// For.
+        /// </summary>
+        For,
+
+        /// <summary>
+        /// Withdrew.
+        /// </summary>
+        Withdrew,
+    }
+}
