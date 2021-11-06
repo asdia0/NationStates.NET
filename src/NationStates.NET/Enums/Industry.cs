071a55fe004b2b22a710257b2591fe9f3fec2976
asdia0 <asdia.the.potato@gmail.com>
2021-11-06 15:53:28 +0800

    Edit Industry docs

--- src/NationStates.NET/Enums/Industry.cs
+++ src/NationStates.NET/Enums/Industry.cs
@@ -6,87 +6,87 @@
     public enum Industry
     {
         /// <summary>
-        /// Defines the the arms industry.
+        /// Arms industry.
         /// </summary>
         Arms_Manufacturing,
 
         /// <summary>
-        /// Defines the the automobile industry.
+        /// Automobile industry.
         /// </summary>
         Automobile_Manufacturing,
 
         /// <summary>
-        /// Defines the the manual labour industry.
+        /// Manual labour industry.
         /// </summary>
         Basket_Weaving,
 
         /// <summary>
-        /// Defines the the meat industry.
+        /// Meat industry.
         /// </summary>
         BeefBased_Agriculture,
 
         /// <summary>
-        /// Defines the the book publishing industry.
+        /// Book publishing industry.
         /// </summary>
         Book_Publishing,
 
         /// <summary>
-        /// Defines the the cheese industry.
+        /// Cheese industry.
         /// </summary>
         Cheese_Exports,
 
         /// <summary>
-        /// Defines the the insurance industry.
+        /// Insurance industry.
         /// </summary>
         DoorToDoor_Insurance_Sales,
 
         /// <summary>
-        /// Defines the the furniture industry.
+        /// Furniture industry.
         /// </summary>
         Furniture_Restoration,
 
         /// <summary>
-        /// Defines the the gambling industry.
+        /// Gambling industry.
         /// </summary>
         Gambling,
 
         /// <summary>
-        /// Defines the the information technology industry.
+        /// Information technology industry.
         /// </summary>
         Information_Technology,
 
         /// <summary>
-        /// Defines the the pizza industry.
+        /// Pizza industry.
         /// </summary>
         Pizza_Delivery,
 
         /// <summary>
-        /// Defines the the retail industry.
+        /// Retail industry.
         /// </summary>
         Retail,
 
         /// <summary>
-        /// Defines the the soda industry.
+        /// Soda industry.
         /// </summary>
         Soda_Sales,
 
         /// <summary>
-        /// Defines the the tourist industry.
+        /// Tourism industry.
         /// </summary>
         Tourism,
 
         /// <summary>
-        /// Defines the the fish industry.
+        /// Fish industry.
         /// </summary>
         Trout_Farming,
 
         /// <summary>
-        /// Defines the the mining industry.
+        /// Mining industry.
         /// </summary>
         Uranium_Mining,
 
         /// <summary>
-        /// Defines the the logging industry.
+        /// Logging industry.
         /// </summary>
         Woodchip_Exports,
     }
