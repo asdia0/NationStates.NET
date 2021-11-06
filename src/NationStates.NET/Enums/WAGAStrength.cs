70644189f48d428ae46c6d9d76600fd97f832caa
asdia0 <asdia.the.potato@gmail.com>
2021-11-06 15:35:33 +0800

    Edit WAGAStrength docs

--- src/NationStates.NET/Enums/WAGAStrength.cs
+++ src/NationStates.NET/Enums/WAGAStrength.cs
@@ -1,7 +1,9 @@
 ﻿namespace NationStates.NET
 {
     /// <summary>
-    /// Types of subcategories for the categories <see cref="WAGACategory.Civil_Rights"/>, <see cref="WAGACategory.Free_Trade"/>, <see cref="WAGACategory.Furtherment_Of_Democracy"/>, <see cref="WAGACategory.Global_Disarmament"/>, <see cref="WAGACategory.International_Security"/>, <see cref="WAGACategory.Moral_Decency"/>, <see cref="WAGACategory.Political_Stability"/> and <see cref="WAGACategory.Social_Justice"/>.
+    /// Defines the types of subcategories for the categories <see cref="WAGACategory.Civil_Rights"/>, <see cref="WAGACategory.Free_Trade"/>,
+    /// <see cref="WAGACategory.Furtherment_Of_Democracy"/>, <see cref="WAGACategory.Global_Disarmament"/>, <see cref="WAGACategory.International_Security"/>,
+    /// <see cref="WAGACategory.Moral_Decency"/>, <see cref="WAGACategory.Political_Stability"/> and <see cref="WAGACategory.Social_Justice"/>.
     /// </summary>
     public enum WAGAStrength
     {
