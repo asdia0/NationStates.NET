f7616d6a2c71d308b3d685086405ec773e54e0a3
asdia0 <asdia.the.potato@gmail.com>
2021-11-11 18:54:35 +0800

    Add properties to Trade

--- src/NationStates.NET/Structs/Trade.cs
+++ src/NationStates.NET/Structs/Trade.cs
@@ -16,10 +16,28 @@
         public string Buyer { get; }
 
         /// <summary>
+        /// Gets the ID of the card traded.
+        /// </summary>
+        [JsonProperty]
+        public long CardID { get; }
+
+        /// <summary>
         /// Gets the price the card was traded at.
         /// </summary>
         [JsonProperty]
         public double Price { get; }
+
+        /// <summary>
+        /// Gets the rarity of the card traded.
+        /// </summary>
+        [JsonProperty]
+        public Rarity Rarity { get; }
+
+        /// <summary>
+        /// Gets the season of the card traded.
+        /// </summary>
+        [JsonProperty]
+        public int Season { get; }
 
         /// <summary>
         /// Gets the name of the seller.
@@ -36,12 +54,18 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="Trade"/> struct.
         /// </summary>
+        /// <param name="cardID">The ID of the card traded.</param>
+        /// <param name="season">The season of the card traded.</param>
+        /// <param name="rarity">The rarity of the card traded.</param>
         /// <param name="buyer">The name of the buyer.</param>
         /// <param name="seller">The name of the seller.</param>
         /// <param name="price">The price the card was traded at.</param>
         /// <param name="timeStamp">The time at which the trade occured.</param>
-        public Trade(string buyer, string seller, double price, DateTime timeStamp)
+        public Trade(long cardID, int season, Rarity rarity, string buyer, string seller, double price, DateTime timeStamp)
         {
+            this.CardID = cardID;
+            this.Season = season;
+            this.Rarity = rarity;
             this.Buyer = buyer;
             this.Seller = seller;
             this.Price = price;
