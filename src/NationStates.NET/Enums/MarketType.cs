964ba3e2df3832e8910fcc760f0262674da1259d
asdia0 <asdia.the.potato@gmail.com>
2021-11-11 16:59:06 +0800

    Add MarketType

---
+++ src/NationStates.NET/Enums/MarketType.cs
@@ -0,0 +1,18 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of markets.
+    /// </summary>
+    public enum MarketType
+    {
+        /// <summary>
+        /// An ask.
+        /// </summary>
+        Ask,
+
+        /// <summary>
+        /// A bid.
+        /// </summary>
+        Bid,
+    }
+}
