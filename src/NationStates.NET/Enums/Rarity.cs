d0ed1672bd1604cd422900ef5a75b1df6579b4e4
asdia0 <asdia.the.potato@gmail.com>
2021-11-11 16:49:58 +0800

    Add Rarity

---
+++ src/NationStates.NET/Enums/Rarity.cs
@@ -0,0 +1,38 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of cards.
+    /// </summary>
+    public enum Rarity
+    {
+        /// <summary>
+        /// Common card.
+        /// </summary>
+        Common,
+
+        /// <summary>
+        /// Uncommon card.
+        /// </summary>
+        Uncommon,
+
+        /// <summary>
+        /// Rare card.
+        /// </summary>
+        Rare,
+
+        /// <summary>
+        /// Ultra-rare card.
+        /// </summary>
+        UltraRare,
+
+        /// <summary>
+        /// Epic card
+        /// </summary>
+        Epic,
+
+        /// <summary>
+        /// Legendary card.
+        /// </summary>
+        Legendary,
+    }
+}
