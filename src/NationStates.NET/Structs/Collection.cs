efb17a426ce6f6b98c5b58d3b790b3f77075ce8d
asdia0 <asdia.the.potato@gmail.com>
2021-11-12 00:15:21 +0800

    Add Collection

---
+++ src/NationStates.NET/Structs/Collection.cs
@@ -0,0 +1,52 @@
+﻿namespace NationStates.NET
+{
+    using Newtonsoft.Json;
+    using System;
+    using static Utility;
+
+    /// <summary>
+    /// Represents a collection of cards.
+    /// </summary>
+    public struct Collection
+    {
+        /// <summary>
+        /// Gets the collection's ID.
+        /// </summary>
+        [JsonProperty]
+        public long ID { get; }
+
+        /// <summary>
+        /// Gets the time at which the collection was last updated.
+        /// </summary>
+        [JsonProperty]
+        public DateTime LastUpdated { get; }
+
+        /// <summary>
+        /// Gets the name of the collection.
+        /// </summary>
+        [JsonProperty]
+        public string Name { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collection"/> struct.
+        /// </summary>
+        /// <param name="id">The collection's ID.</param>
+        /// <param name="lastUpdated">The time at which the collection was last updated.</param>
+        /// <param name="name">The name of the collection.</param>
+        public Collection(long id, DateTime lastUpdated, string name)
+        {
+            this.ID = id;
+            this.LastUpdated = lastUpdated;
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// Gets a JSON string representing the collection.
+        /// </summary>
+        /// <returns>A JSON string representing the collection.</returns>
+        public override string ToString()
+        {
+            return Serialize(this);
+        }
+    }
+}
