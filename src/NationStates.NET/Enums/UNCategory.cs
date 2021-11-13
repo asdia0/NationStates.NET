65305d88a5eb569031de7148aa83644e40677d19
asdia0 <asdia.the.potato@gmail.com>
2021-11-14 03:00:58 +0800

    Add UNCategory

---
+++ src/NationStates.NET/Enums/UNCategory.cs
@@ -0,0 +1,88 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of United Nations resolution categories.
+    /// </summary>
+    public enum UNCategory
+    {
+        /// <summary>
+        /// Removes a resolution from international law.
+        /// </summary>
+        Repeal,
+
+        /// <summary>
+        /// Bookkeeping.
+        /// </summary>
+        Bookkeeping,
+
+        /// <summary>
+        /// Develops industry around the world.
+        /// </summary>
+        Advancement_Of_Industry,
+
+        /// <summary>
+        /// Improves worldwide human and civil rights.
+        /// </summary>
+        Civil_Rights,
+
+        /// <summary>
+        /// Promotes funding and the development of education and the arts.
+        /// </summary>
+        Education_And_Creativity,
+
+        /// <summary>
+        /// Increases the quality of the world's environment, at the expense of industry.
+        /// </summary>
+        Environmental,
+
+        /// <summary>
+        /// Reduces barriers to free trade and commerce.
+        /// </summary>
+        Free_Trade,
+
+        /// <summary>
+        /// Increases democratic freedoms.
+        /// </summary>
+        Furtherment_Of_Democracy,
+
+        /// <summary>
+        /// Slashes worldwide military spending.
+        /// </summary>
+        Global_Disarmament,
+
+        /// <summary>
+        /// Modifies universal standards of healthcare.
+        /// </summary>
+        Health,
+
+        /// <summary>
+        /// Improves world security by boosting police and military budgets.
+        /// </summary>
+        International_Security,
+
+        /// <summary>
+        /// Restricts civil freedoms in the interest of moral decency.
+        /// </summary>
+        Moral_Decency,
+
+        /// <summary>
+        /// Restricts political freedoms in the interest of law and order.
+        /// </summary>
+        Political_Stability,
+
+        /// <summary>
+        /// Legalizes or outlaws recreational drug use.
+        /// </summary>
+        Recreational_Drug_Use,
+
+        /// <summary>
+        /// Enacts uniform standards that protect workers, consumers, and the general public.
+        /// </summary>
+        Regulation,
+
+        /// <summary>
+        /// Reduces income inequality and increase basic welfare.
+        /// </summary>
+        Social_Justice,
+    }
+}
