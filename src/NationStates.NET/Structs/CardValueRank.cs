37aa65ada4c660fdb3ad825caa84102e14f04317
asdia0 <asdia.the.potato@gmail.com>
2021-11-13 18:15:11 +0800

    Add CardValueRank

---
+++ src/NationStates.NET/Structs/CardValueRank.cs
@@ -0,0 +1,41 @@
+﻿namespace NationStates.NET.Structs
+{
+    using Newtonsoft.Json;
+    using static Utility;
+
+    /// <summary>
+    /// Represents a card value rank.
+    /// </summary>
+    public struct CardValueRank
+    {
+        /// <summary>
+        /// Gets the card.
+        /// </summary>
+        public Card Card { get; }
+
+        /// <summary>
+        /// Gets the card's rank.
+        /// </summary>
+        public long Rank { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CardValueRank"/> struct.
+        /// </summary>
+        /// <param name="card">The card.</param>
+        /// <param name="rank">The card's rank.</param>
+        public CardValueRank(Card card, long rank)
+        {
+            this.Card = card;
+            this.Rank = rank;
+        }
+
+        /// <summary>
+        /// Gets a JSON string representing the rank.
+        /// </summary>
+        /// <returns>A JSON string representing the rank.</returns>
+        public override string ToString()
+        {
+            return Serialize(this);
+        }
+    }
+}
