5855c977a9ff422021e00ab81e866404f8d4a236
asdia0 <asdia.the.potato@gmail.com>
2021-11-14 03:01:13 +0800

    Add UNRecreationalDrugUse

---
+++ src/NationStates.NET/Enums/UNRecreationalDrugUse.cs
@@ -0,0 +1,18 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of subcategories for the category <see cref="UNCategory.Recreational_Drug_Use"/>.
+    /// </summary>
+    public enum UNRecreationalDrugUse
+    {
+        /// <summary>
+        /// Legalizes recreational drug use.
+        /// </summary>
+        Legalize,
+
+        /// <summary>
+        /// Outlaws recreational drug use.
+        /// </summary>
+        Outlaw,
+    }
+}
