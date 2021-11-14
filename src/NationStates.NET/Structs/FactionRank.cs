767e8c12293a90b22a8135602c3566b915f2b50d
asdia0 <asdia.the.potato@gmail.com>
2021-11-14 21:23:29 +0800

    Add FactionRank

---
+++ src/NationStates.NET/Structs/FactionRank.cs
@@ -0,0 +1,44 @@
+﻿namespace NationStates.NET
+{
+    using Newtonsoft.Json;
+    using static Utility;
+
+    /// <summary>
+    /// Represents a faction rank.
+    /// </summary>
+    public struct FactionRank
+    {
+        /// <summary>
+        /// Gets the faction's ID.
+        /// </summary>
+        [JsonProperty]
+        public long ID { get; }
+
+        /// <summary>
+        /// Gets the faction's rank.
+        /// </summary>
+        [JsonProperty]
+        public long Rank { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FactionRank"/> struct.
+        /// </summary>
+        /// <param name="id">The faction's ID.</param>
+        /// <param name="rank">The faction's rank.</param>
+        public FactionRank(long id, long rank)
+        {
+            this.ID = id;
+            this.Rank = rank;
+        }
+
+
+        /// <summary>
+        /// Gets a JSON string representing the rank.
+        /// </summary>
+        /// <returns>A JSON string representing the rank.</returns>
+        public override string ToString()
+        {
+            return Serialize(this);
+        }
+    }
+}
