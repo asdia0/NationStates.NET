3b355f5bdf95c40515e46112fa1320be153767dc
asdia0 <asdia.the.potato@gmail.com>
2021-11-14 23:05:20 +0800

    Add NDaySort

---
+++ src/NationStates.NET/Enums/NDaySort.cs
@@ -0,0 +1,68 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of N-Day properties to sort by.
+    /// </summary>
+    public enum NDaySort
+    {
+        /// <summary>
+        /// Score.
+        /// </summary>
+        Score,
+
+        /// <summary>
+        /// Nations.
+        /// </summary>
+        Nations,
+
+        /// <summary>
+        /// Nukes.
+        /// </summary>
+        Nukes,
+
+        /// <summary>
+        /// Shield.
+        /// </summary>
+        Shield,
+
+        /// <summary>
+        /// Production.
+        /// </summary>
+        Production,
+
+        /// <summary>
+        /// Targets.
+        /// </summary>
+        Targets,
+
+        /// <summary>
+        /// Launches.
+        /// </summary>
+        Launches,
+
+        /// <summary>
+        /// Intercepts.
+        /// </summary>
+        Intercepts,
+
+        /// <summary>
+        /// Strikes.
+        /// </summary>
+        Strikes,
+
+        /// <summary>
+        /// Targeted.
+        /// </summary>
+        Targeted,
+
+        /// <summary>
+        /// Incoming.
+        /// </summary>
+        Incoming,
+
+        /// <summary>
+        /// Radiation.
+        /// </summary>
+        Radiation,
+    }
+}
