6ae0cc2655ed1b9556efe36ede0a8a08d277ae07
asdia0 <asdia.the.potato@gmail.com>
2021-11-14 18:17:56 +0800

    Add Specialty

---
+++ src/NationStates.NET/Enums/Specialty.cs
@@ -0,0 +1,28 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of N-Day specialists.
+    /// </summary>
+    public enum Specialty
+    {
+        /// <summary>
+        /// Economic specialists. Can accumulate more Production: their cap is 200% higher.
+        /// </summary>
+        Economic,
+
+        /// <summary>
+        /// Intel specialists. Can finalize targets faster. They have no production advantages.
+        /// </summary>
+        Intel,
+
+        /// <summary>
+        /// Military specialists. Builds nukes faster: They receive 50% more nukes when converting production.
+        /// </summary>
+        Military,
+
+        /// <summary>
+        /// Strategic specialists. Builds shields faster: They receive 50% more shields when converting production.
+        /// </summary>
+        Strategic,
+    }
+}
