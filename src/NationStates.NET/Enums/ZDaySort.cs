fd120e7dde34d6191dfc9307a34c57753358e7bf
asdia0 <asdia.the.potato@gmail.com>
2021-11-15 22:53:03 +0800

    Add ZDaySort

---
+++ src/NationStates.NET/Enums/ZDaySort.cs
@@ -0,0 +1,33 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of Z-Day categories to sort by.
+    /// </summary>
+    public enum ZDaySort
+    {
+        /// <summary>
+        /// Most survivors + no zombies.
+        /// </summary>
+        MostSurvivorsNoZombies,
+
+        /// <summary>
+        /// Most survivors + no quarantine.
+        /// </summary>
+        MostSurvivorsNoQuarantine,
+
+        /// <summary>
+        /// Most survivors.
+        /// </summary>
+        MostSurvivors,
+
+        /// <summary>
+        /// Most zombies.
+        /// </summary>
+        MostZombies,
+
+        /// <summary>
+        /// Most dead.
+        /// </summary>
+        MostDead,
+    }
+}
