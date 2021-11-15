2b7a44a0bfbf8c04ca31942662cb4c27d4e34c92
asdia0 <asdia.the.potato@gmail.com>
2021-11-15 23:00:54 +0800

    Add ZDayRegionRank

---
+++ src/NationStates.NET/Structs/ZDayRegionRank.cs
@@ -0,0 +1,43 @@
+﻿namespace NationStates.NET
+{
+    using Newtonsoft.Json;
+    using static Utility;
+
+    /// <summary>
+    /// Defines a region's Z-Day rank.
+    /// </summary>
+    public struct ZDayRegionRank
+    {
+        /// <summary>
+        /// Gets the region's score.
+        /// </summary>
+        [JsonProperty]
+        public long Score { get; }
+
+        /// <summary>
+        /// Gets the region's name.
+        /// </summary>
+        [JsonProperty]
+        public string Region { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ZDayRegionRank"/> struct.
+        /// </summary>
+        /// <param name="region">The region's name.</param>
+        /// <param name="score">The region's score.</param>
+        public ZDayRegionRank(string region, long score)
+        {
+            this.Region = region;
+            this.Score = score;
+        }
+
+        /// <summary>
+        /// Gets a JSON string representing the region during Z-Day.
+        /// </summary>
+        /// <returns>A JSON string representing the region during Z-Day.</returns>
+        public override string ToString()
+        {
+            return Serialize(this);
+        }
+    }
+}
