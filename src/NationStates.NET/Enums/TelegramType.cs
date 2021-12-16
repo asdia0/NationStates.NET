ffb3c1b545290648be8f0f32e282a19461bef27f
asdia0 <asdia.the.potato@gmail.com>
2021-12-17 03:05:06 +0800

    Add TelegramType

---
+++ src/NationStates.NET/Enums/TelegramType.cs
@@ -0,0 +1,18 @@
+﻿namespace NationStates.NET
+{
+    /// <summary>
+    /// Defines the types of telegrams.
+    /// </summary>
+    public enum TelegramType
+    {
+        /// <summary>
+        /// Recruitment telegram.
+        /// </summary>
+        Recruitment,
+
+        /// <summary>
+        /// Non-recruitment telegram.
+        /// </summary>
+        NonRecruitment,
+    }
+}
