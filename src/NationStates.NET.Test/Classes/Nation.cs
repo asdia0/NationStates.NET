2ca748ade901b89cb5a0519c4cbadb22421c3c2b
asdia0 <asdia.the.potato@gmail.com>
2022-02-02 13:40:03 +0800

    Add reference

---
+++ src/NationStates.NET.Test/Classes/Nation.cs
@@ -0,0 +1,13 @@
+namespace NationStates.NET.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Nation
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
