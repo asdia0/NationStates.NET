ce6ba618beb78fe25b235fb346b8d1b488aeaac0
asdia0 <asdia.the.potato@gmail.com>
2022-02-02 17:50:47 +0800

    Create an example program

--- src/NationStates.NET.Example/Program.cs
+++ src/NationStates.NET.Example/Program.cs
@@ -1,12 +1,51 @@
 ﻿namespace NationStates.NET.Example
 {
+    using NationStates.NET;
     using System;
 
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // With NS.NET, you can interact with your nation.
+            CreateADispatch();
+
+            // You can also get information about nations, regions, the world and the world assembly.
+            GetInformation();
+        }
+
+        public static void CreateADispatch()
+        {
+            // Initialise the nation.
+            Nation n = new("dabberwocky");
+
+            // Set the nation's password.
+            n.Pin = "totallyLegitPassword";
+
+            // Create a dispatch
+            string title = "An interesting title.";
+            string text = "Hello world!";
+            DispatchCategory category = DispatchCategory.Meta;
+            DispatchMeta subCategory = DispatchMeta.Reference;
+
+            n.AddDispatch(title, text, category, subCategory);
+        }
+
+        public static void GetInformation()
+        {
+            // Get a nation's population
+            Nation n = new("dabberwocky");
+            Console.WriteLine(n.Population);
+
+            // Get a region's delegate
+            Region r = new("the united federations");
+            Console.WriteLine(r.Delegate);
+
+            // Get the number of nations in the world.
+            Console.WriteLine(World.NumNations);
+
+            // Get the number of World Assembly Delegates.
+            Console.WriteLine(WorldAssembly.NumDelegates);
         }
     }
 }
