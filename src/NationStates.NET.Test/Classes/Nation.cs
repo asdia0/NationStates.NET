c0fd9b5927074e3f9d31893497cf29c8d48b4760
asdia0 <asdia.the.potato@gmail.com>
2022-02-02 17:31:44 +0800

    Testing is cringe

--- src/NationStates.NET.Test/Classes/Nation.cs
+++ src/NationStates.NET.Test/Classes/Nation.cs
@@ -2,8 +2,11 @@ namespace NationStates.NET.Test
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+    /// <summary>
+    /// Tests the <see cref="Nation"/> class.
+    /// </summary>
     [TestClass]
-    public class Nation
+    public class TNation
     {
         [TestMethod]
         public void TestMethod1()
